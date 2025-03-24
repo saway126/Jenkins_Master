a4938109d4e6ef66b409d1d1169a66f4862f483d
kks <kks@example.com>
2025-03-24 12:27:56 +0900

    qqq03

--- src/main/java/com/example/jenkins_test03/TestController.java
+++ src/main/java/com/example/jenkins_test03/TestController.java
@@ -1,7 +1,9 @@
 package com.example.jenkins_test03;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/test")
 @RestController
 public class TestController {
 }

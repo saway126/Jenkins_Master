1ee7e4f3fbcf484a9e07d8000ae813d32b1ce2c5
kks <kks@example.com>
2025-03-24 12:09:27 +0900

    first commit

---
+++ src/main/java/com/example/jenkins_test03/JenkinsTest03Application.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_test03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTest03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTest03Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkins_test03/JenkinsTest03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_test03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTest03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
